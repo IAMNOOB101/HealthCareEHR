f03e1b1713efbd9c972f7259c18a44673ff14478
Aadarsh Agrawal <agrawalaadarsh387@gmail.com>
2026-04-23 23:29:53 +0530

    build: incremental update to test_chat_flow.js (part 2)

--- backend/test_chat_flow.js
+++ backend/test_chat_flow.js
@@ -1,2 +1,4 @@
 import { connectDB } from './src/config/database.js';
 import { User, Role, Doctor, Patient, PortalUser, Appointment } from './src/models/index.js';
+import { isEligiblePair } from './src/controllers/chat.controller.js';
+import bcrypt from 'bcryptjs';
